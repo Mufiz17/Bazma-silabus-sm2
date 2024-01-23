26da2aedc10d403bc1fe36dfceef6e211cbee73a
Firman-001 <pirman475@smk.belajar.id>
2024-01-23 11:05:49 +0700

    Ini Mission CodeWars

---
+++ codewars/mission-17.js
@@ -0,0 +1,10 @@
+const firstIndex = str.indexOf(c);
+const lastIndex = str.lastIndexOf(c);
+
+if (firstIndex === -1) {
+    return -1;
+} else if (firstIndex === lastIndex) {
+    return 0;
+} else {
+    return lastIndex - firstIndex;
+}
