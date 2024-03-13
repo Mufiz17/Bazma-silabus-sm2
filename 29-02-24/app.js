f74b8549f12b46114fff9cc72e4cd9b559220f1a
Firman-001 <pirman475@smk.belajar.id>
2024-03-13 09:41:41 +0700

    belajar db

--- 29-02-24/app.js
+++ 29-02-24/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const database = require('./db/connection');
+const response = require('./helpers/response')
 
 const app = express();
 
@@ -23,55 +24,56 @@ app.get('/api/todo', (req, res) => {
     database.query('SELECT * FROM todo', (err, results) => {
         if (err) {
             console.log(err);
-        } else {
-            res.json({
-                message: 'success',
-                data: results
-            }, 200);
-        };
+        }
+        response(res, 200, { message: 'Succes get Todos', data: results })
     });
 });
 
-// menambahkan data baru
+// menambahkan data baru dan buat validasi tidak bisa di spam dan data tidak boleh kosong
 app.post('/api/todo', (req, res) => {
     const { title, description } = req.body;
     database.query('INSERT INTO todo (title, description) VALUES (?,?)', [title, description], (err, results) => {
         if (err) {
             console.log(err);
-        } else {
-            res.json({
-                message: 'success'
-            }, 201);
-        };
+        }
+        response(res, 200, { message: 'Succes get Todos', data: results })
     });
 })
 
-// menghapuos data
-
+// menghapus data dengan validasi tidak bisa
 app.delete('/api/todo/:id', (req, res) => {
     const { id } = req.params;
     database.query('DELETE FROM todo WHERE id =?', [id], (err, results) => {
         if (err) {
             console.log(err);
         } else {
-            res.json({
-                message: 'success'
-            }, 201);
+            if (results.length === 0) {
+                response(res, 400, { message: 'Succes get Todos', data: results })
+            }
+            response(res, 200, { message: 'Succes get Todos', data: results })
         };
     });
 })
 
-// update data
+// update data DENGAN VALIDASI DATA TIDAK BOLEH KOSONG DAN KALAU SUDAH ADA ISINYA DI MYSQL MAKA BARU BOLEH KOSONG
 app.put('/api/todo/:id', (req, res) => {
-    const { id } = req.params;
     const { title, description } = req.body;
+    const { id } = req.params;
     database.query('UPDATE todo SET title =?, description =? WHERE id =?', [title, description, id], (err, results) => {
         if (err) {
             console.log(err);
-        } else {
-            res.json({
-                message: 'success'
-            }, 201);
-        };
+        }
+        response(res, 200, { message: 'Succes get Todos', data: results })
+    });
+});
+
+// mencari berdasarkan id
+app.get('/api/todo/:id', (req, res) => {
+    const id = req.params;
+    database.query('SELECT * FROM todo WHERE id =?', [id], (err, results) => {
+        if (err) {
+            console.log(err);
+        }
+        response(res, 200, { message: 'Succes get Todos', data: results })
     });
 })
