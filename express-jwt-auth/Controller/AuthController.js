9fbcdbbcc9b9b29c181c57f740fd0b1f86277e0e
Firman-001 <pirman475@smk.belajar.id>
2024-05-08 14:12:14 +0700

    tugas express-jwt-auth

--- express-jwt-auth/Controller/AuthController.js
+++ express-jwt-auth/Controller/AuthController.js
@@ -4,9 +4,9 @@ const { body, validationResult } = require('express-validator')
 async function register(req, res) {
     const validation = [
         body('name').notEmpty().withMessage("Name is required"),
-        body('email').notEmpty().withMessage("Email is required"),
-        body('password').notEmpty().isPassword().withMessage("Password is required"),
-        body('phone').notEmpty().isPhone().withMessage("Phone is required")
+        body('email').notEmpty().isEmail().withMessage("Email is required"),
+        body('password').notEmpty().withMessage("Password is required"),
+        body('phone').notEmpty().withMessage("Phone is required")
     ]
     await Promise.all(validation.map((v) => v.run(req)));
     const errors = validationResult(req);
@@ -18,7 +18,7 @@ async function register(req, res) {
         return res.status(422).json({
             success: false,
             message: "Validation Error",
-            errors: errMsg
+            error: errMsg
         });
     }
     const { name, email, password, phone } = req.body;
@@ -28,10 +28,9 @@ async function register(req, res) {
             res.status(201).json({
                 success: result.success,
                 message: result.message,
-                data: result.id
             })
         } else {
-            res.status(500).json({ error: result.success })
+            res.status(500).json({ error: result.message })
         }
     } catch (error) {
         return 'Error in registration';
--- express-jwt-auth/Models/AuthModel.js
+++ express-jwt-auth/Models/AuthModel.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 async function registerUser(name, email, password, phone) {
     try {
         const [existingEmailUser] = await connection.query('SELECT * FROM user WHERE email = ?', [email]);
-        if (existingEmailUser > 0) throw new Error("E-mail already in use.");
+        if (existingEmailUser.length > 0) throw new Error("E-mail already in use.");
         const hashedPassword = await bcrypt.hash(password, 10);
         const [newUser] = await connection.query(
             'INSERT INTO user (name, email, password, phone) VALUES (?,?,?,?)', [name, email, hashedPassword, phone]);
@@ -18,3 +18,4 @@ async function registerUser(name, email, password, phone) {
         return 'Error in registration';
     }
 }
+module.exports = { registerUser }
