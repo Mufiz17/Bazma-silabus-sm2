33a1f22d464e56071902db191a0d49f297611cb2
Firman-001 <pirman475@smk.belajar.id>
2024-05-10 15:25:31 +0700

    Project Auth

--- express-jwt-auth/Models/AuthModel.js
+++ express-jwt-auth/Models/AuthModel.js
@@ -6,7 +6,9 @@ async function registerUser(name, email, password, phone) {
     try {
         const [existingEmailUser] = await connection.query('SELECT * FROM user WHERE email = ?', [email]);
         if (existingEmailUser.length > 0) throw new Error("E-mail already in use.");
-        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const hashedPassword = await bcrypt.hash(password, 16);
+
         const [newUser] = await connection.query(
             'INSERT INTO user (name, email, password, phone) VALUES (?,?,?,?)', [name, email, hashedPassword, phone]);
         return {
@@ -18,4 +20,36 @@ async function registerUser(name, email, password, phone) {
         return 'Error in registration';
     }
 }
-module.exports = { registerUser }
+async function loginUser(email, password) {
+    try {
+        const [user] = await connection.query('SELECT * FROM user WHERE email = ?', [email]);
+        if (user.length === 0) {
+            throw new Error('User not found');
+        }
+        const isPasswordValid = await bcrypt.compare(password, user[0].password);
+        if (!isPasswordValid) {
+            throw new Error('Invalid password');
+        }
+        // generate token
+        const createToken = jwt.sign({ email: user[0].email, password: user[0].password }, 'bazmaSecretKey');
+        return { success: true, message: 'Login successful', createToken };
+    } catch (error) {
+        console.error(error);
+        return { success: false, message: error.message };
+    }
+}
+async function getMe(token) {
+    try {
+        const decoded = jwt.verify(token.replace('Bearer ', ''), 'bazmaSecretKey');
+        const userData = {
+            id: decoded.id,
+            username: decoded.username,
+            email: decoded.email
+        }
+        return { success: true, message: 'User data retrieved successfully', data: userData };
+    } catch (error) {
+        console.error(error);
+        return { success: false, message: error.message };
+    }
+}
+module.exports = { registerUser, loginUser, getMe }
--- express-jwt-auth/Routes/AuthRoute.js
+++ express-jwt-auth/Routes/AuthRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../Controller/AuthController');
+const { register, login, me } = require('../Controller/AuthController');
+const verifyToken = require('../Middlewares/VerifyToken');
 
 router.post('/register', register);
+router.post('/login', login);
+router.get('/me', verifyToken, me);
+
 
 module.exports = router;
